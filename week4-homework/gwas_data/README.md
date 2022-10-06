32252bbe9b14c4cd6915f938e1adbf3712226c02
Emma <esteins1@jh.edu>
2022-10-06 16:03:27 -0400

    update

---
+++ week4-homework/gwas_data/README.md
@@ -0,0 +1,17 @@
+Step 1:
+plink --vcf genotypes.vcf --pca 10 #this outputs first 10 principal components
+
+Step 3:
+plink --vcf genotypes.vcf --freq #this gets frequncies #this gives frequency need to make historgram from MAF column
+
+Step 4:
+plink --vcf genotypes.vcf --assoc --pheno CB1908_IC50.txt --allow-no-sex --covar plink.eigenvec --linear --out gwasCB1908
+plink --vcf genotypes.vcf --assoc --pheno GS451_IC50.txt --allow-no-sex --covar plink.eigenvec --linear --out gwasGS451
+
+covariates take out confounding structures (like population) batch correction. only want add liine everything else thats not covariate effect
+
+Step 5:
+Top SNP for CB1908: rs10876043 located on chromosome 12 at position 49190411. TUBA1A is a potential causal gene in the region (~chr12:49184795-49189080). This gene is one of three alpha-tubulin genes in a cluster on chromosome 12. 
+Top SNP for GS451: rs7257475 located on chromosome 19 at position 20372113 which is located near a cluster of Zinc Finger protein encoding genes. 
+
+
---
+++ week4-homework/gwas_data/botxplotCB1908.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#need phenotypes from CB1908_IC50 column in file with same name 
+
+# #using genotype and IC phenotype file use assoc.linear just to find the top SNP. vcf only has sample name and genotypes whereas phenotype only has sample name and phenotype not genotype want genotype and phenotype need to comine those using the sample name
+
+
+#find most signficant SNP to plot, look in gwasIC50.assoc.linear
+#seperate out samples by genotype, look at vcf file
+#get corresponding phenotype, look at IC50 file
+ref = []
+het = []
+alt = []
+file = open("gwasCB1908.assoc.linear")
+pval = []
+snp = []
+bp = []
+chrom = []
+for i in file:
+    new = i.rstrip("\n").split()
+    if new[4] == "ADD":
+        pval.append(float(new[8]))
+        snp.append(new[1])
+        bp.append(new[2])
+        chrom.append(new[0])
+min_value = min(pval) #lowest p val in list
+position = pval.index(min_value) #index of lowest value in the list
+significant = snp[position] #to find the most signficant snp
+sigbp = bp[position]
+sigchrom = chrom[position]
+print(significant) #this prints the most significant snp
+print(sigchrom) #this prints significant snp chromosme
+print(sigbp) #this prints significant snp location
+
+genotypes = []
+sampleid = []
+#loop through vcf file
+genotype_file = open("genotypes.vcf")
+pheno_file = open("CB1908_IC50.txt") 
+for i in genotype_file:
+    if i.startswith("##"): #skip header line
+        continue
+    geno_list = i.rstrip("\n").split("\t")
+    if geno_list[2] == significant:
+        genotypes = geno_list[9:]
+    if i.startswith("#"):
+        sampleid = geno_list[9:]
+        # print(sampleid)
+new_dict = dict(zip(sampleid, genotypes))
+for i in pheno_file:
+    if i.startswith("F"):
+        continue
+    pheno_list = i.rstrip("\n").split("\t")
+    id_A = pheno_list[0]
+    id_B = pheno_list[1]
+    combined =id_A + "_" + id_B
+    if pheno_list[2] == "NA":
+        continue
+    sample_val =float(pheno_list[2])
+    gt = new_dict[combined]
+    if gt == "0/0":
+        ref.append(sample_val)
+    if gt == "0/1":
+        het.append(sample_val)
+    if gt == "1/1":
+        alt.append(sample_val)
+#now plot
+fig, ax = plt.subplots()
+ax.boxplot([ref, het, alt])
+ax.set_xlabel("Genotype")
+ax.set_ylabel("Phenotype")
+plt.xticks([1, 2, 3], ["ref", "het", "alt"])
+fig.savefig("boxplotCB1908.png")
+plt.show()
+
+
+
+
+
+
+
+
+
